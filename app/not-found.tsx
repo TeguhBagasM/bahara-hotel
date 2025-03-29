44db6bf4ad1b30e60e5349306e9cc3f505705e68
Teguh Bagas Mardiansyah <116806781+TeguhBagasM@users.noreply.github.com>
2025-03-29 13:42:37 +0200

    add page not found

---
+++ app/not-found.tsx
@@ -0,0 +1,26 @@
+import Image from "next/image";
+import React from "react";
+import { FaArrowLeft, FaFrown } from "react-icons/fa";
+import Link from "next/link";
+
+const NOT_FOUND_IMAGE_URL = "/images/not-found.png";
+
+export default function NotFound() {
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen bg-white dark:bg-gray-950 text-center">
+      <Image
+        src={NOT_FOUND_IMAGE_URL}
+        alt="Not Found"
+        width={360}
+        height={360}
+        className="h-80 w-80 md:h-96 md:w-96 object-cover"
+      />
+      <h2 className="text-xl flex items-center text-orange-500 mt-4">
+        Oops! Halaman tidak ditemukan. <FaFrown className="ml-2" />
+      </h2>
+      <Link href="/" className="mt-6 flex items-center text-orange-500 font-semibold hover:underline">
+        <FaArrowLeft className="mr-2" /> Kembali ke Beranda
+      </Link>
+    </div>
+  );
+}
