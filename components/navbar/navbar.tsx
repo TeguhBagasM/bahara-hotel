2fe145123f29f247c0a881117688e5597b14b9ac
Teguh Bagas Mardiansyah <116806781+TeguhBagasM@users.noreply.github.com>
2025-03-29 14:01:18 +0200

    fix: warning navbar

--- components/navbar/navbar.tsx
+++ components/navbar/navbar.tsx
@@ -8,7 +8,17 @@ const Navbar = () => {
     <div className="fixed top-0 w-full bg-white shadow-sm z-20">
       <div className="max-w-screen-xl mx-auto flex flex-wrap items-center justify-between p-4">
         <Link href="/">
-          <Image src="/images/logo.png" width={49} height={49} alt="logo" priority />
+          <Image
+            src="/images/logo.png"
+            width={49}
+            height={49}
+            alt="logo"
+            style={{
+              width: 49,
+              height: 49,
+            }}
+            priority
+          />
         </Link>
         <Navlink />
       </div>
